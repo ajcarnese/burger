bfdcce823f14ddddd2bd59a65ea5fa42ba30a324
Tony Carnese <Tony Carnese>
2017-03-22 22:20:03 -0400

    populated sample code into empty files (mostly)

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+### Schema
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured boolean DEFAULT false,
+	date timestamp NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Texas BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whopper with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jive Turkey Burger');
