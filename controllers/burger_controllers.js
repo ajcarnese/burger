2f5c37e19059e68d18381744373a456f5c4cce98
Tony Carnese <Tony Carnese>
2017-03-22 22:36:58 -0400

    test.html & css started, controller fixed

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,50 +1,29 @@
+//controller files are plural
 var express = require("express");
 
+// Import the model (cat.js) to use its database functions.
+var burger = require('../models/burger.js');
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burger: data.burger_name
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    burger.all("burgers", function(data){
+        res.render('index', {burgers: data});
+    });    
 });
 
-router.post("/", function(req, res) {
-  burger.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function() {
-    res.redirect("/");
-  });
+router.get("/new", function(req, res) {
+    res.render('burgers/new');
 });
 
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.post("/create", function(req, res){
+    var cols = ['burger_name'];
+    var vals = [req.body.burger_name];
 
-  console.log("condition", condition);
+    burger.create(cols, vals, function(response){
+        res.redirect('/burgers');
+    });
+})
 
-  burger.update({
-    sleepy: req.body.sleepy
-  }, condition, function() {
-    res.redirect("/");
-  });
-});
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function() {
-    res.redirect("/");
-  });
-});
-
-// Export routes for server.js to use.
 module.exports = router;
